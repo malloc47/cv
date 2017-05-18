041d50191822c540412fd2a884b045fbb2adba0c
Jarrell Waggoner <jwaggoner@groupon.com>
2017-05-17 22:52:51 -0500

    helper to install build deps on arch

---
+++ arch-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo pacman -S texlive-most
+owlman install ttf-exljbris
+owlman install libertine
+sudo pacman -S ttf-linux-libertine
+owlman install ttf-geosans-light
+owlman install ttf-impallari-libre-baskerville
