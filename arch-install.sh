cc358ea0f356193083e1de61284d424ce88db4f0
Jarrell Waggoner <malloc47@gmail.com>
2019-02-27 21:32:07 -0600

    remove old arch installer now that I'm riding the nix train

--- arch-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo pacman -S texlive-most
-owlman install ttf-exljbris
-owlman install libertine
-sudo pacman -S ttf-linux-libertine
-owlman install ttf-geosans-light
-owlman install ttf-impallari-libre-baskerville
